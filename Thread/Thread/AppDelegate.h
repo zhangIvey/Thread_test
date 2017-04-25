6cd26504af7b3ea7134e99b4ce9bb8c64c3b52a8
zhangIvey <zhangze@zhangzedeMac-mini.local>
2017-04-25 23:26:57 +0800

    Project init

---
+++ Thread/Thread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Thread
+//
+//  Created by 张泽 on 2017/4/25.
+//  Copyright © 2017年 张泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Thread/Thread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thread
+//
+//  Created by 张泽 on 2017/4/25.
+//  Copyright © 2017年 张泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
