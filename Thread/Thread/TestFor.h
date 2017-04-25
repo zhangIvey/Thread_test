5487364b00f4e418788513ef950901fc8134f260
zhangIvey <zhangze@zhangzedeMac-mini.local>
2017-04-25 23:29:30 +0800

    Text for update

---
+++ Thread/Thread/TestFor.h
@@ -0,0 +1,13 @@
+//
+//  TestFor.h
+//  Thread
+//
+//  Created by 张泽 on 2017/4/25.
+//  Copyright © 2017年 张泽. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestFor : NSObject
+
+@end
---
+++ Thread/Thread/TestFor.m
@@ -0,0 +1,13 @@
+//
+//  TestFor.m
+//  Thread
+//
+//  Created by 张泽 on 2017/4/25.
+//  Copyright © 2017年 张泽. All rights reserved.
+//
+
+#import "TestFor.h"
+
+@implementation TestFor
+
+@end
