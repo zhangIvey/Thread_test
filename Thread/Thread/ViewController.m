97dc66c7fa8fca7c64b6c6ccfacc588595cc33b4
张泽 <zhangzeStrong@126.com>
2017-04-26 13:43:01 +0800

    add NSThread
    1:  how to made pThread
    2: how to use NSThread
    3: NSThread’s name  、priotiy、
    4：thread’s secure lock

--- Thread/Thread/ViewController.m
+++ Thread/Thread/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <pthread.h>
 
 @interface ViewController ()
+
+@property(nonatomic, retain) NSCondition *condition; //用于对线程进行加锁保护
+@property(nonatomic, retain) NSLock *lock; //用于对线程进行加锁保护
 
 @end
 
@@ -17,10 +21,115 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
     
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(100, 100, 100, 40);
+    [btn setTitle:@"pThread" forState:UIControlStateNormal];
+    [btn setBackgroundColor:[UIColor blueColor]];
+    [btn addTarget:self action:@selector(click_pThread) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
     
-    //测试下
+    UIButton *btn1 = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn1.frame = CGRectMake(100, 150, 100, 40);
+    [btn1 setTitle:@"NSThread" forState:UIControlStateNormal];
+    [btn1 setBackgroundColor:[UIColor blueColor]];
+    [btn1 addTarget:self action:@selector(click_NSThread) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn1];
+    
+    _condition = [[NSCondition alloc] init];
+    _lock = [[NSLock alloc] init];
 }
+
+
+
+
+- (void)click_pThread {
+    
+    [self isMainThread];
+    // 1 : pThread 多线程
+    pthread_t queue;
+    pthread_create(&queue, NULL, runpThread, NULL);
+    
+}
+
+
+- (void) click_NSThread {
+    NSLog(@"主线程打印");
+    // 2 : NSThread 多线程
+    // 初始化方法
+    NSThread *thread1 = [[NSThread alloc] initWithTarget:self selector:@selector(runThread1) object:nil];
+    thread1.name = @"first_NSThread";
+    thread1.threadPriority = 1;
+    
+    
+    
+//    [NSThread detachNewThreadSelector:@selector(runThread1) toTarget:self withObject:nil];
+    
+//    [self performSelectorInBackground:@selector(runThread1) withObject:nil];
+    
+    //设置线程的优先级
+    NSThread *thread2 = [[NSThread alloc] initWithTarget:self selector:@selector(runThread1) object:nil];
+    thread2.name = @"second_NSThread";
+    thread2.threadPriority = 0.5;
+    
+    
+    NSThread *thread3 = [[NSThread alloc] initWithTarget:self selector:@selector(runThread1) object:nil];
+    thread3.name = @"third_NSThread";
+    thread3.threadPriority = 0.2;
+    
+    [thread1 start];
+    [thread2 start];
+    [thread3 start];
+}
+
+
+// 执行子线程
+- (void)runThread1 {
+    
+//    @synchronized (self) { //第一种线程锁 @synchronized
+    
+//    [_condition lock]; //第二种线程锁 NSCondition
+    
+    [_lock lock]; //第三种线程锁 NSLock
+        for (int i = 0; i <= 10 ; i ++) {
+            sleep(1);
+            NSLog(@"当前线程名称：%@ ——%d",[NSThread currentThread].name,i);
+            if (i == 10) {
+                [self performSelectorOnMainThread:@selector(goBackToMain) withObject:nil waitUntilDone:YES];
+            }
+        }
+    [_lock unlock];
+//    [_condition unlock];
+    
+//    }
+    
+}
+
+// 打印主线程信息
+- (void) goBackToMain {
+    NSLog(@"回到主线程");
+}
+
+// 打印是否为当前线程
+- (void)isMainThread {
+    if ([[NSThread currentThread] isMainThread]) {
+        NSLog(@"当前线程是主线程");
+    }else{
+        NSLog(@"当前线程是子线程");
+    }
+    
+}
+
+// C 语言 函数
+void *runpThread(void *data) {
+    for (int i = 0; i < 10 ; i ++) {
+        sleep(1);
+        NSLog(@"%d",i);
+    }
+    return NULL;
+}
+
 
 
 - (void)didReceiveMemoryWarning {
